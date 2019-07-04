aef9357371d0119761c93fa40e8a5adbd6f9a77d
w3cj <cj@null.computer>
2019-07-04 01:39:51 -0600

    💅 Style tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,22 @@
 <script>
 	import sounds from './sounds.json';
 
-	let currentSound = null;
 	let filter = '';
 	let inputBox;
+	let currentSound = null;
+	let filteredSections;
+	let keys = {};
+	let currentKey = 0;
 	
 	const colors = ['#3DDC97', '#FF8C42', '#E87EA1', '#FFD046', '#C45AB3'];
 
 	const audio = new Audio();
-
-	let currentKey = 0;
-	const keyboard = '1234567890qwertyuiopasdfghjkl';
-	const keys = {};
-
-	const sections = Object
-		.keys(sounds)
-		.map(section => {
-			sounds[section].forEach(sound => {
-				sound.key = keyboard[currentKey];
-				keys[keyboard[currentKey]] = {
-					section,
-					sound
-				};
-				currentKey++;
-			});
-			return {
-				section,
-				title: section[0].toUpperCase() + section.slice(1),
-			};
-		});
+	const keyboard = '12345qwertasdfgzxcvb67890yuiop';
 
 	function playSound(section, file) {
 		const path = `sounds/${section}/${file}`;
 		audio.src = path;
+		audio.currentTime = 0;
 		if (section === 'loops') {
 			audio.loop = true;
 		} else {
@@ -41,35 +25,64 @@
 		audio.play();
 	}
 
-	document.addEventListener('keydown', (e) => {
+	function filterBoxKeyDown(e) {
+		if (e.key === 'Escape') {
+			inputBox.blur();
+		}
+	}
+
+	function documentKeyDown(e) {
+		console.log(e);
 		if (e.key === ' ') {
 			audio.pause();
 			audio.currentTime = 0;
 		}
-		if (!keys[e.key]) return;
-		if (inputBox === document.activeElement) return;
+		if (!keys[e.key] || inputBox === document.activeElement) return;
 		const { section, sound: { file, name } } = keys[e.key];
-		playSound(section, file);
-		currentSound = name;
-		setTimeout(() => {
-			currentSound = null;
-		}, 100);
-	});
+		if (currentSound !== name) {
+			playSound(section, file);
+			currentSound = name;
+		}
+	}
 
-	$: filteredSections = Object
+	function documentKeyUp(e) {
+		currentSound = null;
+	}
+
+	audio.addEventListener('ended', () => {
+		currentSound = null;
+	});
+	document.addEventListener('keydown', documentKeyDown);
+	document.addEventListener('keyup', documentKeyUp);
+
+	$: {
+		keys = {};
+		currentKey = 0;
+		filteredSections = Object
 		.keys(sounds)
 		.map(section => {
 			const regexp = new RegExp(filter.trim(), 'i');
 			return {
 				section,
 				title: section[0].toUpperCase() + section.slice(1),
-				sounds: sounds[section].filter(sound => sound.name.match(regexp) || section.match(regexp))
+				sounds: sounds[section].filter(sound => {
+					if (sound.name.match(regexp) || section.match(regexp)) {
+						sound.key = keyboard[currentKey];
+						keys[keyboard[currentKey]] = {
+							section,
+							sound
+						};
+						currentKey++;
+						return true;
+					}
+				})
 			};
 		});
+	}
 </script>
 
 <form class="filter-form">
-	<input bind:this={inputBox} bind:value={filter} class="filter-box">
+	<input bind:this={inputBox} placeholder="filter sounds..." bind:value={filter} on:keydown={filterBoxKeyDown} class="filter-box">
 </form>
 <div class="container">
 	{#each filteredSections as { section, title, sounds }, i}
@@ -95,6 +108,7 @@
 	display: grid;
 	grid-template-columns: repeat(5, 1fr);
 	grid-template-rows: repeat(5, 1fr);
+	grid-gap: 15px 15px;
 	color: #FFF200;
 	margin: 0 auto;
 }
@@ -107,7 +121,7 @@
 }
 
 .filter-box {
-	width: 60%;
+	width: 30%;
 	font-size: 3vmin;
 	font-family: "Allerta Stencil", sans-serif;
 	background: #000F08;
@@ -126,6 +140,7 @@
 	cursor: pointer;
 	box-shadow: 5px 5px 5px #FF4141;
 	transition: background 0.25s linear, color 0.25s linear, box-shadow 0.15s linear;
+	text-transform: uppercase;
 }
 
 .section-title {
